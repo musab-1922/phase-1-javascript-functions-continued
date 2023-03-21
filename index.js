a5f6ef6311d1f14578816abef2273e9a1921a859
musab <musababdi1922@gmail.com>
2023-03-21 08:48:52 +0300

    Completed Assignement

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!` 
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(activity='go to the office'){
+  return `This Monday, I will ${ activity}.`
+}
+mondayWork("work from home");
+function wrapAdjective(wrapper = "*"){
+  return function (msg = "a hard worker"){
+      return `You are ${wrapper}${msg}${wrapper}!`; 
+  }
+}
+console.log(wrapAdjective('||'));
